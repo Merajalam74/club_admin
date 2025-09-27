5123432422f3d8636b614c85f14c3412fa277137
MD MERAJ ALAM <merajalamnazeeri@gmail.com>
2025-09-28 02:27:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use(cors(
+  origin: "*"
+));
 app.use(express.json());
 
 // MongoDB connection
