134e63f72204ed5e7a8cf68e02bd14c93706692e
MD MERAJ ALAM <merajalamnazeeri@gmail.com>
2025-09-28 03:00:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB error:", err));
