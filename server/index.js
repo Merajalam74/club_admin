d784e147761ac24dde19455bdfb7340e27e33a82
MD MERAJ ALAM <merajalamnazeeri@gmail.com>
2025-10-04 16:33:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import csv from "csvtojson";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -243,6 +243,6 @@ app.get("/api/messages", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () =>
-  console.log(`✅ Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
